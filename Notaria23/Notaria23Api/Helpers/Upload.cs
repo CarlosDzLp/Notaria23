927aa567802b11fdaeaf81c177931ed6f2062f4a
CarlosDiaz <carlosdl@grupocybac.com>
2020-05-12 16:35:02 -0500

    add

---
+++ Notaria23/Notaria23Api/Helpers/Upload.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace Notaria23Api.Helpers
+{
+    public static class Upload
+    {
+        private static bool WriteFile(MemoryStream stream, string folder, string name)
+        {
+            try
+            {
+                stream.Position = 0;
+                var path = Path.Combine(folder, name);
+                File.WriteAllBytes(path, stream.ToArray());
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static string ImagePath(byte[] fileArray)
+        {
+            try
+            {
+                var stream = new MemoryStream(fileArray);
+                var guid = Guid.NewGuid().ToString();
+                var file = $"{guid}.png";
+                var folder = $"~/Images/";
+                var folderFull = HttpContext.Current.Server.MapPath(folder);
+                var response = WriteFile(stream, folderFull, file);
+                if (response)
+                {
+                    return $"/Images/{file}";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ Notaria23/Notaria23Api/ViewModels/TokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Notaria23Api.ViewModels
+{
+    public class TokenRequest
+    {
+        public string Token { get; set; }
+        public int Expired { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
