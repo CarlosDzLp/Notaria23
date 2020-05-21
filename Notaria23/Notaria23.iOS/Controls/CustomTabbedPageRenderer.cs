e8c98b68e9437c80a53f7562c9e1e9a065e66799
CarlosDiaz <carlosdl@grupocybac.com>
2020-05-21 14:23:12 -0500

    addd

--- Notaria23/Notaria23.iOS/Controls/CustomTabbedPageRenderer.cs
+++ Notaria23/Notaria23.iOS/Controls/CustomTabbedPageRenderer.cs
@@ -38,11 +38,12 @@ namespace Notaria23.iOS.Controls
 
         UIImage UndeLineColorPosition(UIColor color, CGSize size, CGSize lineSize)
         {
-            var rect = new CGRect(0, 0, size.Width, size.Height);
+            var rect = new CGRect(0, 0, size.Width, size.Height-5);
             var rectLine = new CGRect(0, size.Height - lineSize.Height, lineSize.Width, lineSize.Height);
             UIGraphics.BeginImageContextWithOptions(size, false, 0);
             UIColor.Clear.SetFill();
-            UIGraphics.RectFill(rect); color.SetFill();
+            UIGraphics.RectFill(rect);
+            color.SetFill();
             UIGraphics.RectFill(rectLine);
             var img = UIGraphics.GetImageFromCurrentImageContext();
             UIGraphics.EndImageContext();
---
+++ Notaria23/Notaria23/Controls/KeyboardView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Notaria23.Controls
+{
+    public class KeyboardView : StackLayout
+    {
+        
+    }
+}
