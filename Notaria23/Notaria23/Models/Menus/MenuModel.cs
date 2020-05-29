ac7bf920a307388a15cccd3cbdcdc3c8faa9518b
Carlos Diaz <ryankar90@hotmail.com>
2020-05-29 11:54:10 -0500

    add

---
+++ Notaria23/Notaria23/Models/Menus/MenuModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Notaria23.ViewModels.Base;
+using Xamarin.Forms;
+
+namespace Notaria23.Models.Menus
+{
+    public class MenuModel : BindableNotify
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public Color BackgroundColor { get; set; }
+        public Color TextColor { get; set; }
+        public string Img { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
---
+++ Notaria23/Notaria23/ViewModels/Principal/MenuPageViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Notaria23.Models.Menus;
+using Notaria23.Utils;
+using Notaria23.ViewModels.Base;
+using Notaria23.Views.Principal;
+using Xamarin.Forms;
+
+namespace Notaria23.ViewModels.Principal
+{
+    public class MenuPageViewModel : BindableBase
+    {
+        #region Properties
+        public ObservableCollection<MenuModel> ListMenu { get; set; }
+        #endregion
+
+        #region Constructor
+        public MenuPageViewModel()
+        {
+            LoadMenu();
+            SelectedItemMenuCommand = new Command<MenuModel>(SelectedItemMenuCommandExecuted);
+        }
+        #endregion
+
+        #region Methods
+        private void LoadMenu()
+        {
+            try
+            {
+                ListMenu = new ObservableCollection<MenuModel>();
+                ListMenu.Clear();
+                ListMenu.Add(new MenuModel { BackgroundColor = Color.FromHex("#5B2273"), TextColor = Color.White, Title = "Inicio", ID = 0, Img = FontAwesomeIcons.Home,TargetType=typeof(HomePage) });
+                ListMenu.Add(new MenuModel { BackgroundColor = Color.White, TextColor = Color.FromHex("#5B2273"), Title = "Notaria", ID = 1, Img = FontAwesomeIcons.Notaria, TargetType = typeof(NotaryPage) });
+                ListMenu.Add(new MenuModel { BackgroundColor = Color.White, TextColor = Color.FromHex("#5B2273"), Title = "Tramites en linea", ID = 2, Img = FontAwesomeIcons.Online, TargetType = typeof(OnlinePage) });
+                ListMenu.Add(new MenuModel { BackgroundColor = Color.White, TextColor = Color.FromHex("#5B2273"), Title = "Requisitos", ID = 3, Img = FontAwesomeIcons.Requisitos, TargetType = typeof(DocumentsPage) });
+                ListMenu.Add(new MenuModel { BackgroundColor = Color.White, TextColor = Color.FromHex("#5B2273"), Title = "Directorio", ID = 4, Img = FontAwesomeIcons.Directorio, TargetType = typeof(DirectoryPage) });
+                ListMenu.Add(new MenuModel { BackgroundColor = Color.White, TextColor = Color.FromHex("#5B2273"), Title = "Contacto", ID = 5, Img = FontAwesomeIcons.Contacto, TargetType = typeof(ContactPage) });
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void ResetMenu(MenuModel menu)
+        {
+            foreach(var item in ListMenu)
+            {
+                if(item.ID == menu.ID)
+                {
+                    item.TextColor = Color.White;
+                    item.BackgroundColor = Color.FromHex("#5B2273");
+                }
+                else
+                {
+                    item.BackgroundColor = Color.White;
+                    item.TextColor = Color.FromHex("#5B2273");
+                }
+            }
+        }
+        
+        #endregion
+
+        #region Command
+        public ICommand SelectedItemMenuCommand { get; set; }
+        #endregion
+
+        #region CommandExecuted
+        private void SelectedItemMenuCommandExecuted(MenuModel menuModel)
+        {
+            try
+            {
+                ResetMenu(menuModel);
+                NavigationAsync(menuModel);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
